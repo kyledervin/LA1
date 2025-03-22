8cee0d24476e58312febe0a07a545a6b4516091a
Kyle Dervin <kdervin@arizona.edu>
2025-03-22 16:49:53 -0700

    Fully implemented user class

--- src/main/model/User.java
+++ src/main/model/User.java
@@ -75,4 +75,104 @@ public class User {
     public void setPlayCounts(ArrayList<PlayCount> playCounts) {
         this.playCounts = playCounts;
     }
+    /*
+     * Hashes a password using a simple hash.
+     * 
+     * Parameters: password (type: String) -- the plaintext password
+     * 
+     * Returns: the hashed password
+     */
+    private String hashPassword(String password) {
+        return Integer.toString(password.hashCode());
+    }
+
+    /*
+     * Checks if a given password matches this user's stored and hashed password.
+     * 
+     * Parameters: password (type: String) -- the plaintext password
+     * 
+     * Returns: true if match or false otherwise
+     */
+    public boolean checkPassword(String password) {
+        return this.password.equals(hashPassword(password));
+    }
+
+    /*
+     * Adds a song to this user's library if not already present.
+     * 
+     * Parameters: song (type: Song) -- the song to add
+     * 
+     * Returns: none
+     */
+    public void addSong(Song song) {
+        if (!userSongs.contains(song)) {
+            userSongs.add(song);
+        }
+    }
+
+    /*
+     * Adds an album to this user's library if its not already present. Also adds the album's songs.
+     * 
+     * Parameters: album (type: Album) -- the album to add
+     * 
+     * Returns: none
+     */
+    public void addAlbum(Album album) {
+        if (!userAlbums.contains(album)) {
+            userAlbums.add(album);
+            for (Song song : album.getSongs()) {
+                addSong(song);
+            }
+        }
+    }
+
+    /*
+     * Creates a new playlist if one with the same name does not already exist.
+     * 
+     * Parameters: name (type: String) -- the playlist name
+     * 
+     * Returns: true if created or false otherwise
+     */
+    public boolean createPlaylist(String name) {
+        for (Playlist playlist : playlists) {
+            if (playlist.getName().equalsIgnoreCase(name)) {
+                return false;
+            }
+        }
+        playlists.add(new Playlist(name));
+        return true;
+    }
+
+    /*
+     * Increments the play count for a given song.
+     * 
+     * Parameters: song (type: Song) -- the song played
+     * 
+     * Returns: none
+     */
+    public void incrementPlayCount(Song song) {
+        for (PlayCount pc : playCounts) {
+            if (pc.getSong().equals(song)) {
+                pc.incrementCount();
+                return;
+            }
+        }
+        playCounts.add(new PlayCount(song, 1));
+    }
+
+    /*
+     * Retrieves how many times a given song has been played.
+     * 
+     * Parameters: song (type: Song) -- the song
+     * 
+     * Returns: int play count
+     */
+    public int getPlayCount(Song song) {
+        for (PlayCount pc : playCounts) {
+            if (pc.getSong().equals(song)) {
+                return pc.getCount();
+            }
+        }
+        return 0;
+    }
 }
